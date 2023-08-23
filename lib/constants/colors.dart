bbc58a4816bcc58993ac6828f8a3f9301a0dbc0d
Atikuzzaman Sabuj <sabuj235@gmail.com>
2023-08-23 20:03:19 +0600

    Update Pages #2

--- lib/constants/colors.dart
+++ lib/constants/colors.dart
@@ -3,6 +3,6 @@ import 'package:flutter/material.dart';
 class MainColor {
   static Color primaryColor = Color(0xFFE0F2F1);
   static Color secondaryColor = Color(0xFFffca27);
-  static Color accentColor = Color(0xFF4169e8);
+  static Color accentColor = Color(0xFFE91E63);
   static Color backgroundColor = Color(0xFF00BFA5);
 }
